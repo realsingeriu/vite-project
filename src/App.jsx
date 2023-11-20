b0349279da1b17dae0548815fb9a8b8f5fd586dd
realsingeriu <147463909+realsingeriu@users.noreply.github.com>
2023-11-20 14:41:28 +0900

    TaskForm 컴포넌트 및 CSS 입력

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-
 import './App.css'
 import TaskForm from './components/TaskForm';
+
 
 function App() {
   
   return (
     <div className='app'>
-    <header className='app_header'>Header Section</header>
     <TaskForm />
     <main className='app_main'>
       <section className='task_column'>section 1</section>
--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -1,27 +1,32 @@
-import React from 'react';
+import React from "react";
 
-export default function TaskForm() {
+function TaskForm() {
 	return (
 		<header className='app_header'>
 			<form>
-				<input type='text' className='task_input' placeholder='Enter your task' />
+				<input type='text' className='task_input' placeholder='할일을 입력해 주세요.' />
 
 				<div className='task_form_bottom_line'>
-					<button className='tag'>HTML</button>
-					<button className='tag'>CSS</button>
-					<button className='tag'>JavaScript</button>
-					<button className='tag'>React</button>
-
-					<select className='task_status'>
-						<option value='todo'>할일</option>
-						<option value='todo'>진행중</option>
-						<option value='todo'>완료</option>
-					</select>
-					<button type='submit' className='task_submit'>
-						+ 추가
-					</button>
+					<div>
+						<button className='tag'>HTML</button>
+						<button className='tag'>CSS</button>
+						<button className='tag'>JavaScript</button>
+						<button className='tag'>React</button>
+					</div>
+					<div>
+						<select className='task_status'>
+							<option value='todo'>할일</option>
+							<option value='todo'>진행중</option>
+							<option value='todo'>완료</option>
+						</select>
+						<button type='submit' className='task_submit'>
+							+ 추가
+						</button>
+					</div>
 				</div>
 			</form>
 		</header>
 	);
 }
+
+export default TaskForm
