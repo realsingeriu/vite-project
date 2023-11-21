418c6f52d4db6c22f907742429a4f9920856dcad
realsingeriu <147463909+realsingeriu@users.noreply.github.com>
2023-11-21 10:36:09 +0900

    TaskCard & TaskTag 설정 , 폼스테이트 관리 추가

--- src/components/Tag.jsx
+++ src/components/Tag.jsx
@@ -1,8 +1,20 @@
 import './Tag.css';
 
-export default function Tag({tagName}) {
+export default function Tag({tagName, selectTag, selected}) {
+  const tagStyle = {
+		HTML: { backgroundColor: '#fda821' },
+		CSS: { backgroundColor: '#15d4c8' },
+		JavaScript: { backgroundColor: '#ffd12c' },
+		REACT: { backgroundColor: '#4cdafc' },
+		default: { backgroundColor: '#f9f9f9' },
+	};
+  
   return (
-    <button className='tag'>
+    <button 
+    className='tag' 
+    type='button' 
+    style={selected ? tagStyle[tagName] : tagStyle.default}
+    onClick={() => selectTag(tagName)}>
       {tagName}
     </button>
     
--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -1,24 +1,74 @@
+import { useState } from 'react';
 import Tag from './Tag';
 import './TaskForm.css';
 
 function TaskForm() {
+	const [taskData, setTaskData] = useState({
+		task: '',
+		status: 'todo',
+		tags: [],
+	});
+
+	const handleChange = (e) => {
+		const { name, value } = e.target;
+		setTaskData((prev) => {
+			return { ...prev, [name]: value };
+		});
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		console.log(taskData);
+	};
+
+	//선택한 태그를 tags에 추가한다. (있으면 삭제 없으면 추가)
+	const selectTag = (tag) => {
+		if (taskData.tags.some((item) => item === tag)) {
+			const filterTags = taskData.tags.filter((item) => item !== tag);
+			setTaskData((prev) => {
+				return { ...prev, tags: filterTags };
+			});
+		} else {
+			setTaskData((prev) => {
+				return { ...prev, tags: [...prev.tags, tag] };
+			});
+		}
+	};
+
+		//선택된 태그아이템중 tag가 있으면 true 없으면 false
+		const checkTag = (tag) => {
+			return taskData.tags.some((item) => item === tag);
+		};
+
 	return (
 		<header className='app_header'>
-			<form>
-				<input type='text' className='task_input' placeholder='할일을 입력해 주세요.' />
+			<form onSubmit={handleSubmit}>
+			<input
+					type='text'
+					onChange={handleChange}
+					value={taskData.task}
+					name='task'
+					className='task_input'
+					placeholder='할일을 입력해 주세요.'
+				/>
 				<div className='task_form_bottom_line'>
 					<div>
-						<Tag tagName="HTML" />
-            <Tag tagName="CSS" />
-            <Tag tagName="JavaScript" />
-            <Tag tagName="REACT" />
+						<Tag tagName="HTML" selectTag={selectTag} selected={checkTag('HTML')}/>
+            <Tag tagName="CSS" selectTag={selectTag} selected={checkTag('CSS')}/>
+            <Tag tagName="JavaScript" selectTag={selectTag} selected={checkTag('JavaScript')}/>
+            <Tag tagName="REACT" selectTag={selectTag} selected={checkTag('REACT')}/>
 					</div>
  
 					<div>
-						<select className='task_status'>
+						<select
+							className='task_status'
+							name='status'
+							value={taskData.status}
+							onChange={handleChange}
+						>
 							<option value='todo'>할일</option>
-							<option value='todo'>진행중</option>
-							<option value='todo'>완료</option>
+							<option value='doing'>진행중</option>
+							<option value='done'>완료</option>
 						</select>
 						<button type='submit' className='task_submit'>
 							+ 추가
